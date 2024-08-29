8c4edcdd6428977287159b12c6038fcc91da436c
Marko Grizelj <marko.grizelj@gauss.dev>
2024-08-29 14:04:48 +0200

    fix: made all fields pub

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -2,47 +2,47 @@ use binread::{io::Cursor, BinRead};
 
 #[derive(BinRead)]
 pub struct PartPacked {
-    parent_index: u32,
-    geometry: u32,
-    animation: u32,
+    pub parent_index: u32,
+    pub geometry: u32,
+    pub animation: u32,
 }
 
 #[derive(BinRead)]
 pub struct AnimationInst {
-    t: u16,
-    len: u16,
-    c: u16,
-    d: u16,
+    pub t: u16,
+    pub len: u16,
+    pub c: u16,
+    pub d: u16,
 }
 
 #[derive(BinRead, Debug, Clone)]
 pub struct AnimationFrame {
-    vx: i16,
-    vy: i16,
-    vz: i16,
-    id: u16,
+    pub vx: i16,
+    pub vy: i16,
+    pub vz: i16,
+    pub id: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct AnimationFrames {
-    translation: AnimationFrame,
-    rotation: AnimationFrame,
-    scale: AnimationFrame,
+    pub translation: AnimationFrame,
+    pub rotation: AnimationFrame,
+    pub scale: AnimationFrame,
 }
 
 #[derive(Clone, Debug)]
 pub struct Part {
-    vert_offset: usize,
-    vert_len: usize,
-    tex_offset: usize,
-    tex_len: usize,
-    _animation: usize,
+    pub vert_offset: usize,
+    pub vert_len: usize,
+    pub tex_offset: usize,
+    pub tex_len: usize,
+    pub _animation: usize,
 }
 
 #[derive(BinRead)]
 pub struct Header {
-    texture_offset: u32,
-    _part_count: u32,
+    pub texture_offset: u32,
+    pub _part_count: u32,
     #[br(count=_part_count)]
-    parts: Vec<PartPacked>,
+    pub parts: Vec<PartPacked>,
 }
