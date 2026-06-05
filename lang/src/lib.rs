3d9b33953e00c8237c36c98d35c3c3aae38cc76a
markisha64 <marko.grizelj03@gmail.com>
2026-06-06 00:10:21 +0200

    feat: lang (modified from ddw3)

---
+++ lang/src/lib.rs
@@ -0,0 +1,113 @@
+//! Language files
+
+// Modules
+mod codepoint;
+mod string;
+mod util;
+
+// Exports
+pub use self::{codepoint::Codepoint, string::String};
+
+// Imports
+use {
+    anyhow::Context,
+    byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt},
+    std::io,
+};
+
+/// Lang file
+#[derive(PartialEq, Clone, Debug)]
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct LangFile {
+    /// Strings
+    strings: Vec<String>,
+}
+
+impl LangFile {
+    /// Parses a lang file.
+    pub fn parse<R: io::Read + io::Seek + io::BufRead>(mut reader: R) -> Result<Self, anyhow::Error> {
+        let start = reader.stream_position().context("Unable to get stream position")?;
+
+        // Read the entries
+        let entries_len = reader
+            .read_u32::<LittleEndian>()
+            .context("Unable to read entries length")?;
+        let entries = (0..entries_len)
+            .map(|_| reader.read_u32::<LittleEndian>())
+            .collect::<Result<Vec<_>, io::Error>>()
+            .context("Unable to read entries")?;
+
+        // And read all strings
+        let strings = entries
+            .iter()
+            .map(|&offset| {
+                reader
+                    .seek(io::SeekFrom::Start(start + u64::from(offset)))
+                    .context("Unable to seek to string")?;
+                String::parse_null_terminated(&mut reader).context("Unable to parse string")
+            })
+            .zip(&entries)
+            .map(|(res, &offset)| res.with_context(|| format!("Unable to read string at {offset:#x}")))
+            .collect::<Result<Vec<_>, anyhow::Error>>()?;
+
+        Ok(Self { strings })
+    }
+
+    /// Encodes a lang file.
+    ///
+    /// Returns the size of the file.
+    pub fn encode<W: io::Write + io::Seek>(&self, mut writer: W) -> Result<u64, anyhow::Error> {
+        // Seek past the header
+        // Note: We'll write it later, so we have all the positions of each string
+        let header_len = 4 + 4 * self.strings.len();
+        let header_len = u64::try_from(header_len).context("Header length didn't fit into a `u64`")?;
+        writer
+            .seek(io::SeekFrom::Start(header_len))
+            .context("Unable to seek past header")?;
+
+        // Write all strings and take note of their position
+        // Note: `cur_pos` is relative to the start of this file.
+        let mut cur_pos = header_len;
+        let strings_pos = self
+            .strings
+            .iter()
+            .map(|s| {
+                // Align ourselves to a 4 byte boundary
+                let string_pos = cur_pos.next_multiple_of(4);
+                cur_pos = string_pos;
+                writer
+                    .seek(io::SeekFrom::Start(cur_pos))
+                    .context("Unable to seek to 4-byte boundary")?;
+
+                // Write the string (with the null terminator)
+                for &codepoint in s.iter().chain(&[Codepoint::Null]) {
+                    codepoint
+                        .encode(&mut writer)
+                        .context("Unable to write codepoint to output")?;
+                    cur_pos += u64::from(codepoint.len());
+                }
+
+                Ok(string_pos)
+            })
+            .collect::<Result<Vec<_>, anyhow::Error>>()?;
+
+        // Then go back and write the header
+        writer
+            .seek(io::SeekFrom::Start(0))
+            .context("Unable to seek back to header")?;
+        let strings_len =
+            u32::try_from(self.strings.len()).context("Unable to convert strings' length to `u32`")?;
+        writer
+            .write_u32::<LittleEndian>(strings_len)
+            .context("Unable to write header length")?;
+        for string_pos in strings_pos {
+            let string_pos =
+                u32::try_from(string_pos).context("String position didn't fit into a `u32`")?;
+            writer
+                .write_u32::<LittleEndian>(string_pos)
+                .context("Unable to write string position")?;
+        }
+
+        Ok(cur_pos)
+    }
+}
---
+++ lang/src/string.rs
@@ -0,0 +1,127 @@
+//! String of codepoints
+
+// Imports
+use {
+    crate::Codepoint,
+    anyhow::Context,
+    std::{fmt, io, str::FromStr},
+};
+
+/// String of codepoints
+#[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(serde_with::SerializeDisplay, serde_with::DeserializeFromStr)]
+pub struct String {
+    /// All codepoints
+    codepoints: Vec<Codepoint>,
+}
+
+impl String {
+    /// Returns an iterator over all codepoints
+    pub fn iter(&self) -> Iter<'_> {
+        self.into_iter()
+    }
+
+    /// Parses a null-terminated string.
+    pub fn parse_null_terminated<R: io::BufRead>(mut reader: R) -> Result<Self, anyhow::Error> {
+        // Read until null
+        let mut bytes = vec![];
+        reader
+            .read_until(b'\0', &mut bytes)
+            .context("Unable to read until null")?;
+
+        // Then decode all codepoints
+        let mut codepoints = vec![];
+        let mut bytes = bytes.as_slice();
+        while !bytes.is_empty() {
+            let (codepoint, rest) = Codepoint::decode(bytes).context("Unable to parse bytes")?;
+            if codepoint == Codepoint::Null {
+                break;
+            }
+            codepoints.push(codepoint);
+            bytes = rest;
+        }
+
+        Ok(Self { codepoints })
+    }
+
+    /// Returns the length of this string in bytes
+    #[must_use]
+    pub fn len(&self) -> usize {
+        struct DummyWriter {
+            len: usize,
+        }
+        impl io::Write for DummyWriter {
+            fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+                self.len += buf.len();
+                Ok(buf.len())
+            }
+            fn flush(&mut self) -> io::Result<()> {
+                Ok(())
+            }
+        }
+
+        let mut dummy_writer = DummyWriter { len: 0 };
+        for &codepoint in &self.codepoints {
+            codepoint.encode(&mut dummy_writer).expect("Unable to encode codepoint");
+        }
+        dummy_writer.len
+    }
+
+    /// Returns if this string is empty
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+impl IntoIterator for String {
+    type Item = Codepoint;
+    type IntoIter = std::vec::IntoIter<Codepoint>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.codepoints.into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a String {
+    type IntoIter = Iter<'a>;
+    type Item = &'a Codepoint;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.codepoints.iter()
+    }
+}
+
+impl fmt::Display for String {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for codepoint in &self.codepoints {
+            codepoint.fmt(f)?;
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for String {
+    type Err = anyhow::Error;
+
+    fn from_str(mut s: &str) -> Result<Self, Self::Err> {
+        let mut codepoints = vec![];
+        while !s.is_empty() {
+            let (codepoint, rest) = Codepoint::parse(s)?;
+            codepoints.push(codepoint);
+            s = rest;
+        }
+        Ok(Self { codepoints })
+    }
+}
+
+impl FromIterator<Codepoint> for String {
+    fn from_iter<T: IntoIterator<Item = Codepoint>>(iter: T) -> Self {
+        Self {
+            codepoints: iter.into_iter().collect(),
+        }
+    }
+}
+
+/// Iterator type for [`String`]
+pub type Iter<'a> = std::slice::Iter<'a, Codepoint>;
